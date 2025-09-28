956c2ed9bf07f0b895823b139efbeec802a954f5
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-09-28 12:05:51 +0530

    Add User model with authentication methods

---
+++ backend/src/models/User.js
@@ -0,0 +1,104 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+const bcrypt = require('bcryptjs');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  first_name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      len: [1, 100]
+    }
+  },
+  last_name: {
+    type: DataTypes.STRING(100),
+    allowNull: true,
+    validate: {
+      len: [0, 100]
+    }
+  },
+  email: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+      notEmpty: true
+    }
+  },
+  password_hash: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  risk_appetite: {
+    type: DataTypes.ENUM('low', 'moderate', 'high'),
+    defaultValue: 'moderate',
+    allowNull: false
+  },
+  reset_token: {
+    type: DataTypes.STRING(255),
+    allowNull: true
+  },
+  reset_token_expires: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  tableName: 'users',
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at',
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password_hash) {
+        user.password_hash = await bcrypt.hash(user.password_hash, 12);
+      }
+    },
+    beforeUpdate: async (user) => {
+      if (user.changed('password_hash')) {
+        user.password_hash = await bcrypt.hash(user.password_hash, 12);
+      }
+    }
+  }
+});
+
+// Instance methods
+User.prototype.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password_hash);
+};
+
+User.prototype.toJSON = function() {
+  const values = Object.assign({}, this.get());
+  delete values.password_hash;
+  delete values.reset_token;
+  delete values.reset_token_expires;
+  return values;
+};
+
+// Class methods
+User.findByEmail = function(email) {
+  return this.findOne({ where: { email } });
+};
+
+User.createUser = async function(userData) {
+  return this.create(userData);
+};
+
+module.exports = User;
